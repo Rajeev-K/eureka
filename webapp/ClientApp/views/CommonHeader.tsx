f2c43d22b7a6640340efb265b75ed9150536690b
rajeev-k <rajeev.k@outlook.com>
2019-08-11 10:20:54 -0700

    Add manifest, warning for unsupported browsers

--- webapp/ClientApp/views/CommonHeader.tsx
+++ webapp/ClientApp/views/CommonHeader.tsx
@@ -2,6 +2,6 @@
 
 export function CommonHeader(props: any): JSX.Element {
     return (
-        <h2 className="page-title">eureka<i>!</i></h2>
+        <h2 className="page-title" title="Copyright (c) 2019-present, Rajeev-K">eureka<i>!</i></h2>
     );
 }
