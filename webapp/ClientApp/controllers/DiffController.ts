519931254f98668107cfd9fa85845238857784a1
rajeev-k <rajeev.k@outlook.com>
2019-08-28 22:00:50 -0700

    Check response status before extracting code

--- webapp/ClientApp/controllers/DiffController.ts
+++ webapp/ClientApp/controllers/DiffController.ts
@@ -41,8 +41,8 @@ export class DiffController extends MvcRouter.Controller {
             const p1 = fetch(`/eureka-service/api/git/fileAtRevision?path=${encodeURIComponent(this.path)}&revision=${this.hash1}`);
             const p2 = fetch(`/eureka-service/api/git/fileAtRevision?path=${encodeURIComponent(this.path)}&revision=${this.hash2}`);
             const responses = await Promise.all([p1, p2]);
-            const text1 = await responses[0].text();
-            const text2 = await responses[1].text();
+            const text1 = responses[0].status === 200 ? await responses[0].text() : '';
+            const text2 = responses[1].status === 200 ? await responses[1].text() : '';
             this.diffPage.displayDiff(text1, text2);
         }
         catch (error) {
